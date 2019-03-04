498624d41a10a031bc0db0ebe0f3ed8153e48a97
Alex Lasiuk <alexslasiuk@gmail.com>
2019-03-03 16:11:26 -0800

    Fix reset button.

--- src/reducers/winMessagesReducer.js
+++ src/reducers/winMessagesReducer.js
@@ -1,15 +1,32 @@
 import initialState from './initialState';
 import {UPDATE_WIN_MESSAGE, RESET_WIN_MESSAGE} from '../actions/actionTypes';
 
-export default function winMessages(state = initialState.winMessages, action) {
-  let newState;
+function copyWinMessages(winMessages) {
+  let initWinMessagesState = winMessages;
+  let winMessagesState = Object.assign({}, initWinMessagesState);
+  winMessagesState.pullSummary = Object.assign({}, initWinMessagesState.pullSummary);
+
+  return winMessagesState;
+}
+
+export default function winMessages(state = initialState, action) {
+  if (state.winMessages == null) {
+    let oldState = copyWinMessages(state);
+    state = {};
+    state.winMessages = oldState;
+  }
+
+  let winMessagesState = copyWinMessages(state.winMessages);
+
+  let newState = Object.assign({}, state);
+  newState.winMessages = winMessagesState;
+
   switch (action.type) {
     case UPDATE_WIN_MESSAGE:
-      newState = action.winMessages;
-      return newState;
+      return action.winMessages;
     case RESET_WIN_MESSAGE:
-      return state;
+      return newState.winMessages;
     default:
-      return state;
+      return newState.winMessages;
   }
 }
