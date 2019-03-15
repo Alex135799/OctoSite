1a70f99d8999865745115c3e794b3eab2a7d59ee
Alex Lasiuk <alexslasiuk@gmail.com>
2019-03-15 10:12:15 -0700

    Adding youtube and twich logos with links.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../assets/hanuka_blast.png';
+import yt_icon from '../../assets/youtube_icon.svg';
+import twitch_icon from '../../assets/twitch_icon.svg';
 
 class Home extends Component {
 
@@ -11,15 +13,34 @@ class Home extends Component {
           <p>
             This isn't even Octo's final form!
             <br />
-            <a
-              className="App-link"
-              href="https://www.twitch.tv/smellyoctopus"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <p className="underline">
               Discover your inner Octopus.
-            </a>
+            </p>
           </p>
+          <div className="logos">
+            <div className="logo">&nbsp;</div>
+            <div className="logo">
+              <a
+                className="App-link"
+                href="https://www.twitch.tv/smellyoctopus"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                <img src={twitch_icon} className="twitch-logo" alt="twitch-logo" />
+              </a>
+            </div>
+            <div className="logo" >
+              <a
+                className="App-link"
+                href="https://www.youtube.com/user/Smelly0ctopus"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                <img src={yt_icon} className="youtube-logo" alt="youtube-logo" />
+              </a>
+            </div>
+            <div className="logo">&nbsp;</div>
+          </div>
         </div>
       </div>
     );
