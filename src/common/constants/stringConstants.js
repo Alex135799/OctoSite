2800343db5cc85c3370fdd27ae5984e15995f87d
OoglopCid <ooglopcid@gmail.com>
2020-07-11 15:22:57 -0700

    Adding security to login redirect.

--- src/common/constants/stringConstants.js
+++ src/common/constants/stringConstants.js
@@ -4,7 +4,7 @@ export var queueNoSessionString = "Currently no open sessions...";
 
 let cognitoAuthBase = "https://smellyoctopus.auth.us-east-1.amazoncognito.com/login?"
 let cognitoAuthQueryParams = "client_id=6m4jqicvparrk3i6q637tihotm&response_type=token&scope=email+openid+profile&";
-let cognitoAuthCallbackUrl = "redirect_uri=http://smellyoctopus.com/login";
+let cognitoAuthCallbackUrl = "redirect_uri=https://smellyoctopus.com/login";
 export var cognitoUrl = cognitoAuthBase + cognitoAuthQueryParams + cognitoAuthCallbackUrl;
 
 export var backendUrl = "https://k301suduv8.execute-api.us-east-1.amazonaws.com/default/";
