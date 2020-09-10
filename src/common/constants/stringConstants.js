d24c96de8d8b5befe059a72612964cd23191eb7f
OoglopCid <ooglopcid@gmail.com>
2020-09-10 16:43:46 -0700

    Reverting the callback url from localhost.

--- src/common/constants/stringConstants.js
+++ src/common/constants/stringConstants.js
@@ -5,7 +5,7 @@ export var queueLoadingSessionsString = "Loading sessions...";
 
 let cognitoAuthBase = "https://smellyoctopus.auth.us-east-1.amazoncognito.com/login?"
 let cognitoAuthQueryParams = "client_id=6m4jqicvparrk3i6q637tihotm&response_type=token&scope=email+openid+profile&";
-let cognitoAuthCallbackUrl = "redirect_uri=http://localhost:3000/login";
+let cognitoAuthCallbackUrl = "redirect_uri=https://smellyoctopus.com/login";
 export var cognitoUrl = cognitoAuthBase + cognitoAuthQueryParams + cognitoAuthCallbackUrl;
 
 export var websocketUrl = "wss://0vwmevfi0j.execute-api.us-east-1.amazonaws.com/default";
